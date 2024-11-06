642f36deb80dd1be90f96858406762cab11ed214
Russell Richardson <admin@russ.network>
2024-11-06 10:39:03 -0500

    Implement user image delete endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -18,4 +18,16 @@ router.get('/jobs', security.isAuthenticated, (req, res) => {
     });
 });
 
+router.delete('/image/:imageId', security.isAuthenticated, (req, res) => {
+    db.query('DELETE FROM images WHERE id = ? AND owner_id = ?', [req.params.imageId, req.user.discord_id], (err, results) => {
+        if(err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal Server Error' });
+            return;
+        }
+        // TODO: Socket emit to instruct downstream bots to delete image from known channels.
+        res.status(204).json({ message: 'Image deleted' });
+    });
+});
+
 module.exports = router;
